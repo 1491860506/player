69c5aafc7c9d58fc4b605f227a2228f180169080
1491860506 <81548879+1491860506@users.noreply.github.com>
2022-09-13 17:19:22 +0800

    Add files via upload

--- src/Home.vue
+++ src/Home.vue
@@ -1,27 +1,6 @@
 <template>
     <n-layout>
-        <n-layout-header bordered :class="$style.header">
-            <n-space :class="$style.headerText" justify="center" align="center" :size="[30, 0]">
-                <n-icon size="25" @click="router.back()">
-                    <ArrowBack />
-                </n-icon>
-                <n-icon size="25" @click="router.forward()">
-                    <ArrowForward />
-                </n-icon>
-                <n-icon size="25" @click="refresh">
-                    <Refresh />
-                </n-icon>
-                <n-icon size="25" @click="router.push('/')">
-                    <HomeOutline />
-                </n-icon>
-            </n-space>
 
-            <router-link to="/setting" style="color: inherit;text-decoration: inherit;">
-                <n-icon size="25">
-                    <SettingsOutline />
-                </n-icon>
-            </router-link>
-        </n-layout-header>
         <n-layout-content :class="$style.container" content-style="padding: 24px;">
             <router-view v-slot="{ Component, route }">
                 <keep-alive>
